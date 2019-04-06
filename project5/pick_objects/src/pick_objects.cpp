4ad2d69ec3c0c6480f54cacf4eafecde359a5fc8
Yusen Chen <chenyus@umich.edu>
2019-04-06 15:29:05 +0000

    feat: added a pick_objects pkg and its corresponding .sh code

---
+++ project5/pick_objects/src/pick_objects.cpp
@@ -0,0 +1,74 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+// Define a client to send goal requests to the "move_base" server,
+// through the "SimpleActionClient"
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "pick_objects");
+
+    MoveBaseClient ac("move_base", true);
+
+    // Wait for 5 secs for move_base action server to come up
+    while(!ac.waitForServer(ros::Duration(5.0))){
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
+
+    // Initialize the goal object
+    move_base_msgs::MoveBaseGoal goal;
+
+    // Set up the frame parameters
+    goal.target_pose.header.frame_id = "map";
+    goal.target_pose.header.stamp = ros::Time::now();
+
+    // Define a position and orientation for the robot to reach
+    goal.target_pose.pose.position.x = 1.0;
+    goal.target_pose.pose.orientation.w = 1.0;
+
+    // Send the goal position and orientation for the robot to reach
+    ROS_INFO("Sending goals for the robot to reach");
+    ac.sendGoal(goal);
+
+    // Wait in infinite time for the results
+    ac.waitForResult();
+
+    // Check if the robot reached its goal
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+        ROS_INFO("The base moved 1 meter forward");
+    } else {
+        ROS_INFO("ERROR: The base failed to move forward 1 meter");
+    }
+
+    // Add second goal for the robot to reach
+    goal.target_pose.pose.position.x = 3.0;
+    goal.target_pose.pose.orientation.w = 1.5;
+   
+    ROS_INFO("Sending the second goal for the robot to reach");
+    ac.sendGoal(goal);
+
+    ac.waitForResult();
+
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+        ROS_INFO("The base moved 3 meter forward");
+    } else {
+        ROS_INFO("ERROR: The base failed to move forward 3 meter");
+    }
+
+    // Wait for 5 secs after reaching the pick up zone
+
+    // Trial method one: not working
+    // -------------------------------------------------------------------------
+    // while(!ac.waitForServer(ros::Duration(5.0))){
+    //     ROS_INFO("Waiting for 5 secs after reaching the pick up zone");
+    // }
+    // -------------------------------------------------------------------------
+    
+    // Trial method two
+    ros::Duration(5.0).sleep();
+
+    return 0;
+
+}
---
+++ project5/scripts/pick_objects.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+
+xterm -e " rosrun pick_objects double_objects_picking.launch "
