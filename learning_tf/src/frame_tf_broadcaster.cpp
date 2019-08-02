03eff91294fba7bf05b9506613d17370bc2b7e1d
cyscgzx33 <chenyus@umich.edu>
2019-08-02 13:00:41 -0400

    feat: added a changing frame in frame_tf_broadcaster

--- learning_tf/src/frame_tf_broadcaster.cpp
+++ learning_tf/src/frame_tf_broadcaster.cpp
@@ -11,12 +11,20 @@ int main(int argc, char** argv) {
     ros::Rate rate(10.0);
 
     while ( nh.ok() ) {
-
         // create a new transform, from parent tutle1 to the new child carrot1
         // the carrot1 frame is 2 meters offset to the left from the turtle1 frame
-        transform.setOrigin( tf::Vector3(0.0, 2.0, 0.0) );
+    
+        // a fixed frame that doesn't change over time in relation to the parent frame
+        tf::Transform transform_fixed_frame;
+        transform_fixed_frame.setOrigin( tf::Vector3(0.0, 2.0, 0.0) );
+
+        // a moving frame that changes overtime in relation to the parent frame
+        // Yusen: in the result, carrot1 will moving around turtle1 in a traj of a circle
+        tf::Transform transform_chaging_frame;
+        transform_chaging_frame.setOrigin( tf::Vector3( 2.0*sin(ros::Time::now().toSec() ), 2.0*cos(ros::Time::now().toSec() ), 0.0 ) );
+
         transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
-        br.sendTransform( tf::StampedTransform(transform, ros::Time::now(), "turtle1", "carrot1") );
+        br.sendTransform( tf::StampedTransform(transform_chaging_frame, ros::Time::now(), "turtle1", "carrot1") );
 
         rate.sleep();
     }
